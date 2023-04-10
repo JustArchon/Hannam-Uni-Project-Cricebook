981cac664d8cd4b87b06f4d986ebb55fb2189869
JustArchon <puppy1900@naver.com>
2023-04-10 20:32:51 +0900

    test

---
+++ lib/ui/CircleGroupMain.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CircleGroupMain extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add your idea')),
+      body: Container()
+    );
+  }
+}
---
+++ lib/ui/CircleMain.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CircleMain extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add your idea')),
+      body: Container()
+    );
+  }
+}
