52ecf5a3ab77878cf11279c0021674d7f8451aa8
JustArchon <puppy1900@naver.com>
2023-04-11 13:42:01 +0900

    lib\main.dart: Revert const MyApp(super.key)

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'ui/CircleMain.dart';
-import 'ui/CircleGroupMain.dart';
+import 'ui/Group/CircleGroupMain.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
