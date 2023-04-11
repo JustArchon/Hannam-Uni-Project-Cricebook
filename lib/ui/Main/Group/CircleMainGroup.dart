52a49b9cb25802e183bf418bcfd547225b8f4e5a
JustArchon <puppy1900@naver.com>
2023-04-11 20:01:43 +0900

    lib\ui\Main: Main Page Add

---
+++ lib/ui/Main/Group/CircleMainGroup.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CircleMainGroup extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Circe Book Main Group')),
+      body: Container()
+    );
+  }
+}
---
+++ lib/ui/Main/MainPage/CircleMainPage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CircleMainPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Circe Book Main Page')),
+      body: Container()
+    );
+  }
+}
