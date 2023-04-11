542eac1f6d439da7f97a891f0c992a3dac6741fd
JustArchon <puppy1900@naver.com>
2023-04-11 18:05:06 +0900

    lib\ui\CircleMain.dart: BottomNavigation Add(Win debug not working F)

--- lib/ui/CircleMain.dart
+++ lib/ui/CircleMain.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Group/CircleGroupMain.dart';
 
 class CircleMain extends StatelessWidget{
   @override
@@ -9,3 +10,61 @@ class CircleMain extends StatelessWidget{
     );
   }
 }
+class CircleMainPage extends StatefulWidget {
+  const CircleMainPage({Key? key}) : super(key: key);
+   @override
+   State<CircleMainPage> createState() => CircleMainPageState();
+}
+  
+class CircleMainPageState extends State<CircleMainPage>{
+  int selectedIndex = 0; // 선택된 페이지의 인덱스 넘버 초기화
+
+  final List<Widget> _widgetOptions = <Widget>[
+    CircleGroupMain(),
+  ];
+
+  void onItemTapped(int index) { // 탭을 클릭했을떄 지정한 페이지로 이동
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: SafeArea(child: _widgetOptions.elementAt(selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+          icon: Icon(Icons.text_snippet),
+          label: '메인화면',
+          ),
+          BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: '그룹',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: '프로필'
+          ),
+        ],
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+      ),
+    );
+  }
+  @override
+  void initState() {
+    //해당 클래스가 호출되었을떄
+    super.initState();
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
+
+  
