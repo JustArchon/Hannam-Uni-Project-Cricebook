c630e2eb574c1e2246f39a141fe3c9ff1db08d20
JustArchon <puppy1900@naver.com>
2023-04-16 23:15:48 +0900

    Controller: Firebase Test Page add

---
+++ lib/Controller/FirebaseTest.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FirebaseTest extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Circe Book Firebase Test Page')),
+      body: Container()
+    );
+  }
+}
