0f4d1353f209e837374614b44ffb53a779cbede2
JustArchon <puppy1900@naver.com>
2023-04-21 17:34:11 +0900

    main.dart: SSJ Source merge : ui -> screens Change and main.dart page add by  ssj981113

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:circle_book/screens/group_main_screen.dart';
+import 'package:circle_book/screens/main_group_screen.dart';
+import 'package:circle_book/screens/main_books_screen.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-
-import 'ui/Main/CircleMain.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -13,19 +15,88 @@ void main() async{
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    /*
-      initialRoute: '/',
-      routes: {
-        '/': (context) => CircleMain(),
-        '/GroupMain': (context) => CircleGroupMain()
-      },
-    */
-    debugShowCheckedModeBanner: false,
-    home: CircleMain(),
+      title: "MyApp",
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      // 웹,앱 모두 드래그 작동하게하는 코드
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown
+        },
+      ),
+      home: const MyWidget(),
     );
   }
 }
 
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: TabPage(),
+      ),
+    );
+  }
+}
+
+class TabPage extends StatefulWidget {
+  const TabPage({super.key});
+
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0; // 처음에 나올 화면 지정
+
+  // 이동할 페이지
+  final List _pages = [
+    MainBooksScreen(),
+    const MainGroupScreen(),
+    const GroupMainScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: _pages[_selectedIndex], // 페이지와 연결
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          //type: BottomNavigationBarType.fixed, // bottomNavigationBar item이 4개 이상일 경우
+
+          onTap: _onItemTapped,
+
+          currentIndex: _selectedIndex,
+
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.menu_book_rounded), label: "Books"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.groups_rounded), label: "Group"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_rounded), label: "Profile"),
+          ],
+        ));
+  }
+
+  void _onItemTapped(int index) {
+    // state 갱신
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
