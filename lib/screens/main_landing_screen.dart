c0a902632c0eec3db4219d883ea65967c932c673
JustArchon <puppy1900@naver.com>
2023-04-29 17:43:42 +0900

    landing screen: add timer and move page

--- lib/screens/main_landing_screen.dart
+++ lib/screens/main_landing_screen.dart
@@ -1,17 +1,36 @@
+import 'dart:async';
+import 'package:circle_book/screens/main_login_screen.dart';
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+
 
 class LandingPage extends StatefulWidget {
   _LandingPageState createState() => _LandingPageState();
 }
 
 class _LandingPageState extends State<LandingPage>{
+@override
+  void initState() {
+  	Timer(Duration(milliseconds: 1500), () {
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => const LoginScreen()
+                  )
+                  );
+                });
+  }
   @override
   Widget build(BuildContext conext){
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Image.asset('assets/image/LandingPage.jpg',fit: BoxFit.cover,)
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Image.asset('assets/image/LandingPage.jpg',fit: BoxFit.cover,)
+          ),
+          const CircularProgressIndicator()
+        ],
       )
     );
   }
