18562f53b498ffd35316a038c22a9b499391d168
JustArchon <puppy1900@naver.com>
2023-04-29 17:42:56 +0900

    main.dart,main_screen: main screen separation main.dart: Landing Page in the further process Inevitably, the main page was separated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:circle_book/screens/group_main_screen.dart';
-import 'package:circle_book/screens/main_group_screen.dart';
-import 'package:circle_book/screens/main_books_screen.dart';
-import 'package:flutter/gestures.dart';
+import 'package:circle_book/screens/main_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
+import 'package:circle_book/screens/main_landing_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,86 +17,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: "MyApp",
-      debugShowCheckedModeBanner: false,
-
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      // ��,�� ��� �巡�� �۵��ϰ��ϴ� �ڵ�
-      scrollBehavior: const MaterialScrollBehavior().copyWith(
-        dragDevices: {
-          PointerDeviceKind.mouse,
-          PointerDeviceKind.touch,
-          PointerDeviceKind.stylus,
-          PointerDeviceKind.unknown
-        },
-      ),
-      home: const MyWidget(),
+  Widget build(Object context) {
+    return MaterialApp(
+      home: LandingPage(),
     );
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: TabPage(),
-      ),
-    );
-  }
-}
-
-class TabPage extends StatefulWidget {
-  const TabPage({super.key});
-
-  @override
-  _TabPageState createState() => _TabPageState();
-}
-
-class _TabPageState extends State<TabPage> {
-  int _selectedIndex = 0; // ó���� ���� ȭ�� ����
-
-  // �̵��� ������
-  final List _pages = [
-    MainBooksScreen(),
-    const MainGroupScreen(),
-    const GroupMainScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-          child: _pages[_selectedIndex], // �������� ����
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          //type: BottomNavigationBarType.fixed, // bottomNavigationBar item�� 4�� �̻��� ���
-
-          onTap: _onItemTapped,
-
-          currentIndex: _selectedIndex,
-
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(Icons.menu_book_rounded), label: "Books"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.groups_rounded), label: "Group"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person_rounded), label: "Profile"),
-          ],
-        ));
-  }
-
-  void _onItemTapped(int index) {
-    // state ����
-    setState(() {
-      _selectedIndex = index;
-    });
   }
 }
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:circle_book/screens/group_main_screen.dart';
+import 'package:circle_book/screens/main_group_screen.dart';
+import 'package:circle_book/screens/main_books_screen.dart';
+import 'package:flutter/gestures.dart';
+import 'package:get/get.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+  @override
+  _MainScreenState createState() => _MainScreenState();
+
+}
+
+class _MainScreenState extends State<MainScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MyApp",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      // ��,�� ��� �巡�� �۵��ϰ��ϴ� �ڵ�
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown
+        },
+      ),
+      home: const MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: TabPage(),
+      ),
+    );
+  }
+}
+
+class TabPage extends StatefulWidget {
+  const TabPage({super.key});
+
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0; // ó���� ���� ȭ�� ����
+
+  // �̵��� ������
+  final List _pages = [
+    MainBooksScreen(),
+    const MainGroupScreen(),
+    const GroupMainScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: _pages[_selectedIndex], // �������� ����
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          //type: BottomNavigationBarType.fixed, // bottomNavigationBar item�� 4�� �̻��� ���
+
+          onTap: _onItemTapped,
+
+          currentIndex: _selectedIndex,
+
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.menu_book_rounded), label: "Books"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.groups_rounded), label: "Group"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_rounded), label: "Profile"),
+          ],
+        ));
+  }
+
+  void _onItemTapped(int index) {
+    // state ����
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
