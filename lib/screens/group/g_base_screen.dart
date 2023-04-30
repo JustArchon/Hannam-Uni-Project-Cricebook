7a294464d64d15b69055a7eca761d1ef88608987
JustArchon <puppy1900@naver.com>
2023-04-30 13:59:03 +0900

    lib/screens/group: Group Screen move by ssj981113

---
+++ lib/screens/group/g_base_screen.dart
@@ -0,0 +1,76 @@
+import 'package:circle_book/screens/group/g_main_screen.dart';
+import 'package:circle_book/screens/group/g_chat_screen.dart';
+import 'package:circle_book/screens/group/g_calendar_screen.dart';
+import 'package:circle_book/screens/group/g_board_screen.dart';
+
+import 'package:flutter/material.dart';
+
+class GroupBaseScreen extends StatefulWidget {
+  final String id, title, thumb;
+
+  const GroupBaseScreen({
+    Key? key,
+    required this.id,
+    required this.title,
+    required this.thumb,
+  }) : super(key: key);
+
+  @override
+  State<GroupBaseScreen> createState() => _GroupBaseScreenState();
+}
+
+class _GroupBaseScreenState extends State<GroupBaseScreen> {
+  int _selectedIndex = 0; // 처음에 나올 화면 지정
+
+  late final List<Widget> _pages; // late 키워드로 나중에 초기화
+
+  @override
+  void initState() {
+    super.initState();
+
+    // initState에서 _pages 리스트 초기화
+    _pages = [
+      GroupMainScreen(id: widget.id, title: widget.title, thumb: widget.thumb),
+      GroupChatScreen(id: widget.id, title: widget.title, thumb: widget.thumb),
+      GroupCalendarScreen(
+          id: widget.id, title: widget.title, thumb: widget.thumb),
+      GroupBoardScreen(id: widget.id, title: widget.title, thumb: widget.thumb),
+      // 다른 페이지 추가
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _pages[_selectedIndex], // 페이지와 연결
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType
+            .fixed, // bottomNavigationBar item이 4개 이상일 경우
+
+        onTap: _onItemTapped,
+
+        currentIndex: _selectedIndex,
+
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.groups_rounded), label: "Main"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.chat_rounded), label: "Chat"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month_rounded), label: "Calendar"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.note_alt_outlined), label: "Board"),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    // state 갱신
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
