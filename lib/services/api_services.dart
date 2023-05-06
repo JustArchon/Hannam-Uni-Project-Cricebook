7006fcc38e4ea70df140f8e3cc49a78945b98d0f
JustArchon <puppy1900@naver.com>
2023-05-06 22:26:57 +0900

    api_services.dart: Search Function Add

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -7,6 +7,7 @@ import 'package:circle_book/models/book_model.dart';
 var filter = ['만화', '수험서', '대학교재', '참고서', '달력', '화집', '잡지', '게임', '어린이', '여행', '외국어', '유아', '전집', '컴퓨터', 'Gift'];
 var category = ['2105', '74', '1', '656', '987'];
 //고전, 역사, 소설/시/희곡, 인문학, 과학
+
 //maxResults 말고는 수정하지 마세요.
 class ApiService {
   final String ttbkey = "ttbkimgi06281904001";
@@ -69,6 +70,7 @@ class ApiService {
     return bookInstances;
   }
   
+  //isbn으로 검색해서 이미지 가져오는 기능
   Future<String> getThumb(String bookISBN) async {
     Uri url = 
       Uri.parse('http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey=$ttbkey&itemIdType=ISBN&ItemId=$bookISBN&Cover=Big&output=js&Version=20131101');
@@ -78,6 +80,25 @@ class ApiService {
     if(response.statusCode == 200) {
       final book = BookModel.fromJson(jsonDecode(response.body));
       return book.thumb;
+    }
+    throw Error();
+  }
+
+  Future<List<BookModel>> searchByName(String name) async {
+    List<BookModel> bookInstances = [];
+    Uri url = Uri.parse(
+      'https://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=$ttbkey&Query=$name&QueryType=Title&MaxResults=$maxResults&Cover=Big&output=js&version=20131101');
+    
+    final response = await http.get(url);
+
+    if(response.statusCode == 200) {
+      var responseBody = json.decode(response.body);
+      for(var temp in responseBody['item']) {
+        var book = BookModel.fromJson(temp);
+        book.replaceHTMLEntity();
+        bookInstances.add(book);
+      }
+      return bookInstances;
     }
     throw Error();
   }
