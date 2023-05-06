7ab22e78543488cf4c0e8f6772005a111889f548
JustArchon <puppy1900@naver.com>
2023-05-06 22:50:55 +0900

    models/book_model.dart: book model Update by ssj981113

--- lib/models/book_model.dart
+++ lib/models/book_model.dart
@@ -2,12 +2,12 @@
 //https://docs.google.com/document/d/1mX-WxuoGs8Hy-QalhHcvuV17n50uGI2Sg_GHofgiePE/edit#heading=h.wzyr9o7mof2u
 
 class BookModel {
-  final String title; //책제목
-  final String thumb; //책 커버 image
-  final String id; //isbn
-  final String description; //설명
-  final String categoryName; //카테고리
-  final String author; //지은이
+  String title; //책제목
+  String thumb; //책 커버 image
+  String id; //isbn
+  String description; //설명
+  String categoryName; //카테고리
+  String author; //지은이
 
   BookModel.fromJson(Map<String, dynamic> json)
       : title = json['title'],
@@ -16,4 +16,32 @@ class BookModel {
         thumb = json['cover'],
         id = json['isbn13'],
         description = json['description'];
+
+  void replaceHTMLEntity() {
+    description = description.replaceAll(
+        RegExp(
+          r'&lt',
+        ),
+        '<');
+    description = description.replaceAll(
+        RegExp(
+          r'&gt',
+        ),
+        '>');
+    description = description.replaceAll(
+        RegExp(
+          r'&nbsp',
+        ),
+        ' ');
+    description = description.replaceAll(
+        RegExp(
+          r'&amp',
+        ),
+        '&');
+    description = description.replaceAll(
+        RegExp(
+          r'&amp',
+        ),
+        '"');
+  }
 }
