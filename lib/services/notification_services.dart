71dbcb289fe71f9415ecce2f06f1dbeff85afaaa
JustArchon <puppy1900@naver.com>
2023-06-08 23:01:16 +0900

    noti commit

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -8,7 +8,7 @@ class CircleBook_Notification_Service {
 
   static init() async {
     AndroidInitializationSettings androidInitializationSettings =
-        const AndroidInitializationSettings('mipmap/ic_launcher');
+        const AndroidInitializationSettings('mipmap/circle_book_icon');
 
     DarwinInitializationSettings iosInitializationSettings =
         const DarwinInitializationSettings(
@@ -36,7 +36,7 @@ class CircleBook_Notification_Service {
         );
   }
 
-  static Future<void> showNotification() async {
+  static Future<void> showCertNotification(String Groupname) async {
     const AndroidNotificationDetails androidNotificationDetails =
         AndroidNotificationDetails('channel id', 'channel name',
             channelDescription: 'channel description',
@@ -49,6 +49,54 @@ class CircleBook_Notification_Service {
         iOS: DarwinNotificationDetails(badgeNumber: 1));
 
     await flutterLocalNotificationsPlugin.show(
-        0, '써클북 테스트', '테스트중입니다.', notificationDetails);
+        0, '써클 북', '$Groupname에서 곧 독서인증 기간입니다.', notificationDetails);
+  }
+
+  static Future<void> showBookStartNotification(String Groupname) async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('channel id', 'channel name',
+            channelDescription: 'channel description',
+            importance: Importance.max,
+            priority: Priority.max,
+            showWhen: false);
+
+    const NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails,
+        iOS: DarwinNotificationDetails(badgeNumber: 1));
+
+    await flutterLocalNotificationsPlugin.show(
+        0, '써클북 테스트', '$Groupname에서 그룹독서가 시작되었습니다.', notificationDetails);
+  }
+
+  static Future<void> showBookEndNotification(String Groupname) async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('channel id', 'channel name',
+            channelDescription: 'channel description',
+            importance: Importance.max,
+            priority: Priority.max,
+            showWhen: false);
+
+    const NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails,
+        iOS: DarwinNotificationDetails(badgeNumber: 1));
+
+    await flutterLocalNotificationsPlugin.show(
+        0, '써클 북', '$Groupname에서 그룹독서가 종료되었습니다.', notificationDetails);
+  }
+
+  static Future<void> showShareNotification(String Groupname) async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('channel id', 'channel name',
+            channelDescription: 'channel description',
+            importance: Importance.max,
+            priority: Priority.max,
+            showWhen: false);
+
+    const NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails,
+        iOS: DarwinNotificationDetails(badgeNumber: 1));
+
+    await flutterLocalNotificationsPlugin.show(
+        0, '써클 북', '$Groupname에서 독후감 공유가 가능합니다.', notificationDetails);
   }
 }
