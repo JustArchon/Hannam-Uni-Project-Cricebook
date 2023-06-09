9c832e50f027d0e70901576ea55eeb2d8c163928
JustArchon <puppy1900@naver.com>
2023-06-09 10:32:38 +0900

    GroupProfie: New Profile UI

--- lib/screens/group/g_profile_screen.dart
+++ lib/screens/group/g_profile_screen.dart
@@ -37,74 +37,214 @@ class _GroupProfilePageState extends State<GroupProfilePage> {
                 child: CircularProgressIndicator(),
               );
             } else {
-              return Container(
-                width: MediaQuery.of(context).size.width,
-                padding:
-                    const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 100,
-                      height: 100,
-                      child: Image.asset('assets/icons/usericon.png'),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  '닉네임 : ',
+              String profileIcon = snapshot.data!['profileIcon'];
+
+              return SingleChildScrollView(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.3,
+                        height: MediaQuery.of(context).size.width * 0.3,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 5,
+                            color: snapshot.data!['reputationscore'] > 900
+                                ? Colors.purple
+                                : snapshot.data!['reputationscore'] > 600
+                                    ? Colors.blue
+                                    : snapshot.data!['reputationscore'] > 300
+                                        ? Colors.green
+                                        : snapshot.data!['reputationscore'] > 100
+                                            ? Colors.yellow
+                                            : Colors.red,
+                          ),
+                        ),
+                        child: Center(
+                          child: Image.asset(
+                            'assets/$profileIcon.png',
+                            width: MediaQuery.of(context).size.width * 0.15,
+                            height: MediaQuery.of(context).size.width * 0.15,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                        },
+                        child: RichText(
+                          text: TextSpan(
+                            text: snapshot.data!['userName'],
+                            style: const TextStyle(
+                              fontFamily: "Ssurround",
+                              fontSize: 20,
+                              color: Color(0xff6DC4DB),
+                            ),
+                            children: const <TextSpan>[
+                              TextSpan(
+                                  text: ' 님',
                                   style: TextStyle(
+                                      fontFamily: "Ssurround",
+                                      fontSize: 20,
+                                      color: Colors.black))
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Image.asset(
+                                      'assets/icons/아이콘_상태표시바용(512px).png',
+                                      width: 35,
+                                      height: 35),
+                                  const SizedBox(width: 5),
+                                  GestureDetector(
+                                    onTap: () {
+                                      
+                                    },
+                                    child: const Text(
+                                      '개인소개',
+                                      style: TextStyle(
+                                        fontFamily: "Ssurround",
+                                        fontSize: 20,
+                                        color: Color(0xff6DC4DB),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 7,
+                              ),
+                                Text(
+                                  snapshot.data!["selfintroduction"],
+                                  style: const TextStyle(
                                     fontFamily: "Ssurround",
-                                    fontSize: 20,
+                                    fontSize: 18,
                                     color: Colors.black,
                                   ),
                                 ),
-                                Text(
-                                  snapshot.data!['userName'],
-                                  style: const TextStyle(
-                                    fontFamily: "Ssurround",
-                                    fontSize: 20,
-                                    color: Colors.black,
-                                  ),
-                                )
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  '이메일 : ',
-                                  style: TextStyle(
-                                    fontFamily: "Ssurround",
-                                    fontSize: 20,
-                                    color: Colors.black,
-                                  ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Image.asset(
+                                          'assets/icons/아이콘_상태표시바용(512px).png',
+                                          width: 35,
+                                          height: 35),
+                                      const SizedBox(width: 5),
+                                      const Expanded(
+                                        child: Text(
+                                          '완료한 독서 그룹',
+                                          style: TextStyle(
+                                            fontFamily: "Ssurround",
+                                            fontSize: 20,
+                                            color: Color(0xff6DC4DB),
+                                          ),
+                                        ),
+                                      ),
+                                      Text(
+                                        "${"총 ${snapshot.data!["readingbookcount"]}"}건 완료",
+                                        style: const TextStyle(
+                                          fontFamily: "Ssurround",
+                                          fontSize: 16,
+                                          color: Colors.black,
+                                        ),
+                                      )
+                                    ]),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Image.asset(
+                                        'assets/icons/아이콘_상태표시바용(512px).png',
+                                        width: 35,
+                                        height: 35),
+                                    const SizedBox(width: 5),
+                                    const Text(
+                                      '업적',
+                                      style: TextStyle(
+                                        fontFamily: "Ssurround",
+                                        fontSize: 20,
+                                        color: Color(0xff6DC4DB),
+                                      ),
+                                    ),
+                                      const Expanded(
+                                        child: SizedBox(width: 1)
+                                      ),
+                                      Text(
+                                        "${"총 ${snapshot.data!["complete_Achievements"].length}"}건 완료",
+                                        style: const TextStyle(
+                                          fontFamily: "Ssurround",
+                                          fontSize: 16,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                  ],
                                 ),
-                                Text(
-                                  snapshot.data!['userEmail'],
-                                  style: const TextStyle(
-                                    fontFamily: "Ssurround",
-                                    fontSize: 20,
-                                    color: Colors.black,
-                                  ),
-                                )
-                              ],
+                              ),
+                              SizedBox(height: 10,),
+                              SizedBox(
+                                height: MediaQuery.of(context).size.height * 0.2,
+                                child: GridView.count(
+                                    crossAxisCount: 6,
+                                    crossAxisSpacing: 10,
+                                    mainAxisSpacing: 5,
+                                    children: List.generate(
+                                        snapshot.data!['mounted_Achievements']
+                                            .length, (index) {
+                                      return Image.asset(
+                                          "${"assets/medals/" + snapshot.data!['mounted_Achievements'][index]}.png");
+                                      /*Ink(
+                                          decoration: ShapeDecoration(
+                                          shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(10),
+                                          side: const BorderSide(
+                                          color: const Color(0xff6DC4DB),
+                                          width: 2,
+                                        )),
+                                        ),
+                                          child: 
+                                          );
+                                          */
+                                    })),
+                              ),
+                            ] // 이곳을 기점으로 위젯추가하면됩니다.
                             ),
-                          ]),
-                    ),
-                  ],
+                      ),
+                    ],
+                  ),
                 ),
               );
             }
