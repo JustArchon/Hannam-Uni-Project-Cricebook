425447ac580b9f701acd8e948a6c6f06b42c73c5
JustArchon <puppy1900@naver.com>
2023-06-11 11:45:20 +0900

    Setting: small Dialog Add

--- lib/screens/main/m_setting_screen.dart
+++ lib/screens/main/m_setting_screen.dart
@@ -228,7 +228,57 @@ class MainSettingsScreen extends StatelessWidget {
                                 height: 40,
                               ),
                               GestureDetector(
-                                onTap: () {},
+                                onTap: () {
+                                  Future.delayed(Duration.zero, () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        content: const Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Text(
+                                              '공지사항',
+                                              style: TextStyle(
+                                                fontSize: 18,
+                                                letterSpacing: 1.0,
+                                                fontFamily: "SsurroundAir",
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              '써킅톡이 런칭되었습니다.',
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                letterSpacing: 1.0,
+                                                fontFamily: "SsurroundAir",
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              '모두 매너있는 독서를 하시길 기원합니다.',
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                letterSpacing: 1.0,
+                                                fontFamily: "SsurroundAir",
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        actions: [
+                                          IconButton(
+                                            icon: const Icon(Icons.close),
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                });
+                                },
                                 child: const Row(
                                   mainAxisAlignment:
                                       MainAxisAlignment.spaceBetween,
@@ -310,7 +360,75 @@ class MainSettingsScreen extends StatelessWidget {
                                 height: 30,
                               ),
                               GestureDetector(
-                                onTap: () {},
+                                onTap: () {
+                                  Future.delayed(Duration.zero, () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        content: const Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Text(
+                                              '이용약관 및 정책',
+                                              style: TextStyle(
+                                                fontSize: 18,
+                                                letterSpacing: 1.0,
+                                                fontFamily: "SsurroundAir",
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              '여러분을 환영합니다. 써클톡 서비스 및 제품(이하 ‘서비스’)을 이용해 주셔서 감사합니다.',
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                letterSpacing: 1.0,
+                                                fontFamily: "SsurroundAir",
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              '본 약관은 다양한 써클톡 서비스의 이용과 관련하여 써클톡 서비스를 제공하는 써클톡 주식회사(이하 ‘써클톡’)와 이를 이용하는 써클톡 서비스 회원(이하 ‘회원’) 또는 비회원과의 관계를 설명하며,',
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                letterSpacing: 1.0,
+                                                fontFamily: "SsurroundAir",
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              '써클톡 서비스 및 제품(이하 ‘서비스’)을 이용해 주셔서 감사합니다.  아울러 여러분의 써클톡 서비스 이용에 도움이 될 수 있는 유익한 정보를 포함하고 있습니다.',
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                letterSpacing: 1.0,
+                                                fontFamily: "SsurroundAir",
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              '써클톡 서비스를 이용하시거나 써클톡 서비스 회원으로 가입하실 경우 여러분은 본 약관 및 관련 운영 정책을 확인하거나 동의하게 되므로, 잠시 시간을 내시어 주의 깊게 살펴봐 주시기 바랍니다.',
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                letterSpacing: 1.0,
+                                                fontFamily: "SsurroundAir",
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        actions: [
+                                          IconButton(
+                                            icon: const Icon(Icons.close),
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                });
+                                },
                                 child: const Row(
                                   mainAxisAlignment:
                                       MainAxisAlignment.spaceBetween,
